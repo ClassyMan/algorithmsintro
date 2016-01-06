042356b1c121919e03aeee5871722d4b22956678
Aidan <yardy_14@hotmail.com>
2016-01-06 07:06:44 -0800

    cleaning up

--- algorithms-and-data-structures/src/test/java/algorithms/and/data/structures/TestInsertionSort.java
+++ algorithms-and-data-structures/src/test/java/algorithms/and/data/structures/TestInsertionSort.java
@@ -2,8 +2,6 @@ package algorithms.and.data.structures;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import javax.print.attribute.standard.MediaSize.ISO;
-
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -12,13 +10,13 @@ public class TestInsertionSort {
 	@DataProvider(name = "test")
 	public static Object[][][] arrays(){
 		return new Integer[][][]{{{1, 5, 4, 7, 3, 5, 7, 3, 5, 6} , {1, 3, 3, 4, 5, 5, 5, 6, 7, 7}}, 
-							  {{31, 41, 59, 26, 41, 58}, {26, 31, 41, 41, 58, 59}}}; // Exercise 2.1-1
+						        {{31, 41, 59, 26, 41, 58}, {26, 31, 41, 41, 58, 59}}}; // Exercise 2.1-1
 	}
 	
 	@DataProvider(name = "testDesc")
 	public static Object[][][] arraysDescending(){
 		return new Integer[][][]{{{1, 5, 4, 7, 3, 5, 7, 3, 5, 6} , {7, 7, 6, 5, 5, 5, 4, 3, 3, 1}}, 
-							  {{31, 41, 59, 26, 41, 58}, {59, 58, 41, 41, 31, 26}}}; // Exercise 2.1-2
+							    {{31, 41, 59, 26, 41, 58}, {59, 58, 41, 41, 31, 26}}}; // Exercise 2.1-2
 	}
 	
 	/**
