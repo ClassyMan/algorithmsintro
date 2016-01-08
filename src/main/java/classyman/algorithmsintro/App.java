ce0875247e3c04ac1e0ffcd067e3a1e8e1319a86
Aidan <yardy_14@hotmail.com>
2016-01-08 08:50:30 -0800

    mightve finally ressurected my git setup

---
+++ src/main/java/classyman/algorithmsintro/App.java
@@ -0,0 +1,13 @@
+package classyman.algorithmsintro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
