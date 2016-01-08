e86418b91f874dd0c06b922daf65cd46925c1b2e
Aidan <yardy_14@hotmail.com>
2016-01-08 08:54:51 -0800

    Recommitting the efficiency and sort type classes into new repo

---
+++ src/main/java/classyman/algorithmsintro/algorithms/EfficiencyType.java
@@ -0,0 +1,18 @@
+package classyman.algorithmsintro.algorithms;
+
+public interface EfficiencyType {
+
+	/**
+	 * Calculates efficiency for this type of sorting algorithm.
+	 * 
+	 * @param numberOfIterations
+	 *            The number of iterations we are calculating efficiency for.
+	 * @return the efficiency.
+	 * 
+	 *         TODO: are we really calculating "efficiency" here or do we really
+	 *         mean we are calculating the time taken for this algorithm to
+	 *         return a result.
+	 */
+	Double calculateEfficiencyForCandN(int numberOfIterations);
+
+}
