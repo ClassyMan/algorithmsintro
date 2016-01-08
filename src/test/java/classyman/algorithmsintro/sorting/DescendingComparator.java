e7edb8e11d678136a01037fe585d0d269b994f29
Aidan <yardy_14@hotmail.com>
2016-01-08 09:14:26 -0800

    inclusion of descendingComparator

---
+++ src/test/java/classyman/algorithmsintro/sorting/DescendingComparator.java
@@ -0,0 +1,25 @@
+package classyman.algorithmsintro.sorting;
+
+import java.util.Comparator;
+
+/**
+ * A comparator for integers which need to be sorted in descending order.
+ * 
+ * @author Aidan
+ */
+public final class DescendingComparator implements Comparator<Integer> {
+
+	/**
+	 * Static instantiator
+	 */
+	public static DescendingComparator newDescendingcomparator() {
+		return new DescendingComparator();
+	}
+
+	/*
+	 * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
+	 */
+	public int compare(Integer o1, Integer o2) {
+		return o2 - o1;
+	}
+}
