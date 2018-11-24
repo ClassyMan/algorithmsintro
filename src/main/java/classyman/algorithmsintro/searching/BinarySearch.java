0828dda3c61da3ac86653955107033d5b5f55e58
Aidan Yardy <aidany@chp.co.uk>
2018-11-24 13:13:54 -0500

    adding some bin search comments

--- src/main/java/classyman/algorithmsintro/searching/BinarySearch.java
+++ src/main/java/classyman/algorithmsintro/searching/BinarySearch.java
@@ -13,8 +13,8 @@ public class BinarySearch extends AbstractSearch {
 	public static BinarySearch construct() {
 		return new BinarySearch();
 	}
-	
-	/* 
+
+	/*
 	 * @see classyman.algorithmsintro.searching.AbstractSearch#find(java.lang.Integer)
 	 */
 	@Override
@@ -22,56 +22,56 @@ public class BinarySearch extends AbstractSearch {
 		input = MergeSort.construct().sort(input);
 		return searchIteration(elementToFind, Math.floorDiv(input.length,2));
 	}
-	
+
 	/**
-	 * Iteration of the binary search algorithm. Search for the element at the search position. 
-	 * If it is not there and the comparison element is greater than the element to find then 
-	 * we search left, otherwise we search to the right. 
+	 * Iteration of the binary search algorithm. Search for the element at the search position.
+	 * If it is not there and the comparison element is greater than the element to find then
+	 * we search left, otherwise we search to the right.
 	 */
 	private SearchResult searchIteration(Integer elementToFind, Integer searchPosition) {
-		if (searchPosition > input.length - 1) {
+		if (searchPosition > input.length - 1) { // Validate that the search position is not off the end of the array. (to the right)
 			return new SearchResult(null, input.length - 1).outsideRange();
-		} else if (searchPosition < 0) {
+		} else if (searchPosition < 0) { // Valudate that the search position is not off the end of the array but to the left.
 			return new SearchResult(null, 0).outsideRange();
 		} else if (previouslySearchedPositions[1] == searchPosition) { // we are looking for the element between two numbers where it does not exist
 			int searchPositionTemp = searchPosition;
-			if (elementToFind > input[searchPositionTemp]) {
+			if (elementToFind > input[searchPositionTemp]) { // if our element to find is greater than the element at the search position
 				while(true) {
-					if (elementToFind > input[searchPositionTemp]){
-						searchPositionTemp++;
-					} else if (elementToFind < input[searchPositionTemp]) {
-						return new SearchResult(null, searchPositionTemp-1);
+					if (elementToFind > input[searchPositionTemp]){ // if the element to find is STILL greater than the element at the search position
+						searchPositionTemp++; // Increment the search position
+					} else if (elementToFind < input[searchPositionTemp]) { // Otherwise if the element to find is less than the search position
+						return new SearchResult(null, searchPositionTemp-1); // Then return the search position minus 1
 					}
 				}
-			} else if (elementToFind < input[searchPositionTemp]) {
+			} else if (elementToFind < input[searchPositionTemp]) { // Else if the element to find is less than the search position
 				while(true) {
-					if (elementToFind < input[searchPositionTemp]){
-						searchPositionTemp--;
-					} else if (elementToFind > input[searchPositionTemp]) {
-						return new SearchResult(null, searchPositionTemp+1);
+					if (elementToFind < input[searchPositionTemp]){ // And the element to find is STILL less than the search position
+						searchPositionTemp--; // decrement the temporary search position
+					} else if (elementToFind > input[searchPositionTemp]) { // Else if the element to find is greater than the search position
+						return new SearchResult(null, searchPositionTemp+1); // return the search position + 1
 					}
 				}
 			}
-			return new SearchResult(null, Math.min(previouslySearchedPositions[0], previouslySearchedPositions[1]));
+			return new SearchResult(null, Math.min(previouslySearchedPositions[0], previouslySearchedPositions[1])); // So if the result is between two numbers, just choose the lowest number
 		} else if (previouslySearchedPositions[0] == searchPosition) { // we are looking for the element outside of the range
 			return new SearchResult(null, searchPosition).outsideRange();
 	    } else {
 	    	previouslySearchedPositions[1] = previouslySearchedPositions[0]; // Shift our search positions right
-	    	previouslySearchedPositions[0] = searchPosition; // then add our most recent one. We only care about the last two. 
+	    	previouslySearchedPositions[0] = searchPosition; // then add our most recent one. We only care about the last two.
 	    }
-		
-		Integer comparisonElement = input[searchPosition];
+
+		Integer comparisonElement = input[searchPosition]; // Possible match?
 		int nextDistance = Math.floorDiv(Math.abs(previouslySearchedPositions[0] - previouslySearchedPositions[1]), 2);
 		if(nextDistance == 0){
 			nextDistance = 1;
 		}
-		
-		if (elementToFind < comparisonElement) {
-			return searchIteration(elementToFind, searchPosition - nextDistance);
-		} else if (elementToFind > comparisonElement) {
-			return searchIteration(elementToFind, searchPosition + nextDistance);
+
+		if (elementToFind < comparisonElement) { // If the element to find is a smaller number than the comparison element
+			return searchIteration(elementToFind, searchPosition - nextDistance); // Then return a new search iteration to look in the area below, using a distance of half the last distance
+		} else if (elementToFind > comparisonElement) { // Same here but where the amount is greater
+			return searchIteration(elementToFind, searchPosition + nextDistance); // The return a new search iteration to look in the area above, using a distance of half the last distance
 		} else {
-			return new SearchResult(comparisonElement, searchPosition);
+			return new SearchResult(comparisonElement, searchPosition); // Otherwise, we found it.
 		}
 	}
 }
--- src/test/java/classyman/algorithmsintro/searching/TestBinarySearch.java
+++ src/test/java/classyman/algorithmsintro/searching/TestBinarySearch.java
@@ -14,34 +14,34 @@ public class TestBinarySearch {
 	@DataProvider(name = "testSearch")
 	public static Object[][] arrays(){
 		return new TestSearchData[][]{{new TestSearchData(new Integer[]{1, 5, 4, 7, 3, 5, 7, 3, 5, 6}, 3, 2)},
-						              {new TestSearchData(new Integer[]{31, 41, 59, 26, 41, 58}, 26, 0)}, 
+						              {new TestSearchData(new Integer[]{31, 41, 59, 26, 41, 58}, 26, 0)},
 						              {new TestSearchData(new Integer[]{11, 23, 59, 26, 44, 200, 300, 100, 214, 500, 400}, 400, 9)}};
 	}
-	
+
 	@DataProvider(name = "testSearchNotFound")
 	public static Object[][] notFound(){
 		return new TestSearchData[][]{{new TestSearchData(new Integer[]{1, 5, 4, 7, 3, 5, 7, 3, 5, 6}, 3525, 9).outsideRange()},
-			                          {new TestSearchData(new Integer[]{10, 10, 10, 10, 10, 10}, 1, 0).outsideRange()}, 
-			                          {new TestSearchData(new Integer[]{1, 1, 1, 1, 1, 1}, 10, 5).outsideRange()}, 
-						              {new TestSearchData(new Integer[]{31, 41, 59, 26, 41, 58}, 10, 0).outsideRange()}, 
+			                          {new TestSearchData(new Integer[]{10, 10, 10, 10, 10, 10}, 1, 0).outsideRange()},
+			                          {new TestSearchData(new Integer[]{1, 1, 1, 1, 1, 1}, 10, 5).outsideRange()},
+						              {new TestSearchData(new Integer[]{31, 41, 59, 26, 41, 58}, 10, 0).outsideRange()},
 						              {new TestSearchData(new Integer[]{31, 41, 59, 26, 41, 58}, 45, 3)},
 						              {new TestSearchData(new Integer[]{11, 23, 59, 26, 44, 200, 300, 100, 214, 500, 400}, 401, 9)}
-						              
+
 		};
 	}
-	
+
 	/**
 	 * Tests that for a given input we can find the value we desire
 	 */
 	@Test(dataProvider = "testSearch")
 	public void testSearch(TestSearchData testSearchData) {
 		SearchResult searchResult = BinarySearch.construct().withInput(testSearchData.input).find(testSearchData.elementToFind);
-		
+
 		assertThat(searchResult).isNotNull();
 		assertThat(searchResult.result).isEqualTo(testSearchData.elementToFind);
 		assertThat(searchResult.positionInArray).isEqualTo(testSearchData.expectedPosition);
 	}
-	
+
 	/**
 	 * Tests what happens when the object is not found
 	 */
